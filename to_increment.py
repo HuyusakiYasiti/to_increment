f815e53ddd4bf423d83333f5baa0dce7960e38e7
CROW_07 <100528047+HuyusakiYasiti@users.noreply.github.com>
2022-11-03 17:13:01 +0900

    Update to_increment.py

--- to_increment.py
+++ to_increment.py
@@ -1,47 +1,21 @@
 import time
 
-
-
-
 def main():
-
     n = RequestNum()
-
-
     for i in range(n):
-
         i = i + 1
-
         time.sleep(1)
-
         print(i)
 
-
-
-
 def RequestNum():
-
     r = input("Please input number.: ")
-
-
     try:
-
         r = int(r)
-
     except ValueError:
-
         print("Please type using half-width characters and enter an integer.")
-
         r = RequestNum()
-
     finally:
-
         return r
 
-
-
-
 if __name__ == "__main__":
-
     main()
-
